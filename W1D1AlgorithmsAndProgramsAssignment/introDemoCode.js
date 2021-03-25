eaaf8b3f5b992a92f26aab084576005115dae890
michaelgalazar <79475036+michaelgalazar@users.noreply.github.com>
2021-03-25 02:08:36 -0700

    update

--- W1D1AlgorithmsAndProgramsAssignment/introDemoCode.js
+++ W1D1AlgorithmsAndProgramsAssignment/introDemoCode.js
@@ -1,4 +1,25 @@
-"use strict"
-const number = 5;
-number = number + number;
-console.log(number);
+"use strict";
+
+/* eslint-disable */
+// const number = 5;
+// number = number + number;
+// console.log(number);
+
+function reverseMove(string, integer) {
+    let inputString = string;
+    let inputArray = [];
+    inputArray = inputString.split("");
+
+    for (let i = 0; i < integer; i++) {
+        inputArray.unShift(inputArray.pop());
+    }
+    for (let j = 0; j < inputArray.length; j++) {
+        for (let k = 0; k < inputArray[i].length; k++) {
+            inputArray[i].reverse() = inputArray[i].split("");
+            inputArray[i].join("");
+        }
+    }
+    return inputArray.join(" ");
+}
+
+console.log(reverseMove("I am great", 3));
